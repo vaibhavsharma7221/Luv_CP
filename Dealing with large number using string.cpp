5f84b597c3e361dc1f8a3917c04f37a0d4fa1f68
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-03-23 17:42:13 +0530

    Create Dealing with large number using string.cpp

---
+++ Dealing with large number using string.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    
+    int last_digit=s[s.size()-1]-'0';
+    
+    cout<<"last digit :"<<last_digit<<"\n";
+    return 0;
+}
