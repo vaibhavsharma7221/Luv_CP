f7b2b6b362f7149d93c493c0e7ef800581c339b4
Vaibhav_7221 <76476010+vaibhavsharma7221@users.noreply.github.com>
2022-03-23 17:37:44 +0530

    Create cin.ignore fxn.cpp

---
+++ cin.ignore fxn.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        string s;
+        cin.ignore();
+        getline(cin,s);
+        cout<<s;
+    }
+}
